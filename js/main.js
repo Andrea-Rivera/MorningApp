27c2c0ac10123d73cc993a86b968a7f1ded89679
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2022-11-07 14:27:37 +1000

    Adding Web Api

--- js/main.js
+++ js/main.js
@@ -15,35 +15,45 @@ console.log(greetingTwo.toUpperCase());
 let message = "Today is a sunny day!";
 console.log(message.toLowerCase());
 
-function coffee() {
-  alert("You clicked me! Ok, Let me have my coffee first.");
-}
-
-function header() {
-  let head = document.querySelector("h1");
-  head.style.textTransform = "uppercase";
-  head.style.color = "#FF4D00";
-}
-
-function removeQuote() {
-  document.getElementById("phrase").innerHTML = "";
-}
-
-//lesson 3
 function countingWords(str) {
   if (str === null || "") {
     return 0;
   }
   return str.length;
 }
+console.log(countingWords("Andrea"));
+//console.log(str); //Error because str is a local variable.
+
+//Lesson 3
+function coffee() {
+  alert("You clicked me! Ok, Let me have my coffee first.");
+}
+
+setInterval(greetingFunction, 1000);
+
+function greetingFunction() {
+  var greetingOption = document.getElementById("greeting");
+  if (greetingOption.innerHTML === "Hello") {
+    greetingOption.innerHTML = "Good Morning";
+  } else {
+    greetingOption.innerHTML = "Hello";
+  }
+}
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+}
+function removeQuote() {
+  document.getElementById("phrase").innerHTML = "";
+}
 
 function myFunction() {
   document.getElementById("demo").innerHTML =
     "Believe you can, and you're halfway there.";
 }
-
-console.log(countingWords("Andrea"));
-//console.log(str); //Error because str is a local variable.
 
 function jsFunction(value) {
   if (value === "Shopping") {
@@ -56,4 +66,18 @@ function jsFunction(value) {
     document.getElementById("answer").innerHTML =
       "Have a coffee and let's do it.";
   }
+}
+
+const activity_btn = document.getElementById("activity_btn");
+activity_btn.addEventListener("click", getRandomActivity);
+
+const activity_result = document.getElementById("activity_result");
+
+function getRandomActivity() {
+  fetch("https://www.boredapi.com/api/activity")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      activity_result.innerHTML = `${data.activity}`;
+    });
 }
